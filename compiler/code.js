cddcdf634af0c580458aa1093e355cbef253cc09
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-03-12 15:15:42 +0900

    Update code.js

--- compiler/code.js
+++ compiler/code.js
@@ -1,6 +1,9 @@
 import regIndexes from './reg-indexes.js';
 
 const code=text=>{
+  text=text
+    .replaceAll("`","\\`")
+    .replaceAll("~","\\~");
   const indexes=regIndexes(/^-{3,5}[^-\n\r]*$/mg,text);
   indexes.reverse().forEach(index=>{
     const line=text.slice(index.start,index.end);
