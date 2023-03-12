abece424a146434af4aa1f35062adb92c629b261
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-03-12 12:28:23 +0900

    add write

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@ import compile from "./compiler/index.js";
 async function file(path,id){
   const text=await Deno.readTextFile(path);
   const result=compile(text,id);
-  
+  await Deno.writeTextFile(`./dist/${id}.md`,text);
 }
 for(let i=1;i!==65;i++){
   const path=`./wizardbible/${i}/${i}.txt`;
