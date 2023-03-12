76315527e81b4b0e403635693c292471b7e90ecd
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-03-12 15:17:43 +0900

    Update title.js

--- compiler/title.js
+++ compiler/title.js
@@ -1,4 +1,6 @@
 const title=(text,id)=>{
+  text=text.replaceAll("=","\\=")
+    .replaceAll("-","\\-");
   text=text.split("\r\n").slice(5).join("\r\n");
   text=`# Wizardbible #${id}`+text;
   return text;
