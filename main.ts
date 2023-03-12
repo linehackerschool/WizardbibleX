2f5d9e2255109c30fb8a1abb0c45ba2fa1c4ec76
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-03-12 12:23:10 +0900

    fix

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 //download
-await Deno.run({
+const clone=Deno.run({
   cmd:["git","clone","https://github.com/linehackerschool/wizardbible"],
 });
+await clone.status();
 for(let i=1;i!==65;i++){
   console.log(i)
 }
