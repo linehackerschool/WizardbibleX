d166fb3db8f165a5e3073bbb59aa3b2d6ef37edb
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-03-12 12:27:17 +0900

    fix

--- main.ts
+++ main.ts
@@ -3,7 +3,14 @@ const clone=Deno.run({
   cmd:["git","clone","https://github.com/linehackerschool/wizardbible"],
 });
 await clone.status();
+
+import compile from "./compiler/index.js";
+async function file(path,id){
+  const text=await Deno.readTextFile(path);
+  const result=compile(text,id);
+  
+}
 for(let i=1;i!==65;i++){
   const path=`./wizardbible/${i}/${i}.txt`;
-  console.log(path)
+  file(path,i)
 }
